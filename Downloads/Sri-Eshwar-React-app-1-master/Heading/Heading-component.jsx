f29f09cddb153eb065b01e15f8da12fc95a502b7
sachit <sachit.k2024cse@sece.ac.in>
2025-12-03 12:15:26 +0530

    new file is added

---
+++ Downloads/Sri-Eshwar-React-app-1-master/Heading/Heading-component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { HeadingComponentStyle } from "./Heading-component-style";
+const HeadingComponent = () => {
+  return (
+    <HeadingComponentStyle>
+      <div className="capsule">
+        <h1 className="heading">A Symbol of Excellence in Placement Training</h1>
+        <p className="sub__heading">
+          Aptitude Guru Hem was born from the aspiration to create a learning environment that inspires greatness. Our founder, driven by personal experiences and a desire to make a difference, established the company with a mission to empower students with the right skills and mindset for success.
+        </p>
+      </div> 
+    </HeadingComponentStyle>
+  );
+};
+export default HeadingComponent;
