ba6f1266778a568ddaac915c9d161ccf37d84315
sachit <sachit.k2024cse@sece.ac.in>
2025-12-08 09:21:26 +0530

    DAY_5

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import FlexBoxComponent from "./flex-box/flex-box-component";
+import FlexWrapComponent from "./flex-wrap/flex-wrap-component";
+import HelloWorldComponent from "./hello-world/hello-world-component";
+import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+import OurPromisesComponent from "./our-promises/our-promises";
+import PositionExampleComponent from "./position-example/position-example-component";
+import PostionComponent from "./positioning/position-component";
+import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
+import TrustedbyComponent from "./trusted-by/trusted-by-component";
+
+function App() {
+  return (
+    <div>
+      {/* <HelloWorldComponent /> */}
+      {/* <FlexBoxComponent /> */}
+      {/* <LeadersTomorrowComponent /> */}
+      {/* <SymbolOfExcellenceComponent /> */}
+      {/* <PostionComponent /> */}
+      {/* <TrustedbyComponent /> */}
+      {/* <FlexWrapComponent /> */}
+      <OurPromisesComponent />
+      {/* <PositionExampleComponent /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/our-promises/our-promises.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { OurPromisesStyle } from "./our-promises-style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { OurpromisesData } from "./data";
+
+const OurPromisesComponent = () => {
+  return (
+    <OurPromisesStyle>
+      <div className="paras">
+        <h1>Our Promise</h1>
+        <p>
+          As part of our high quality service, we'd like to offer something
+          extra too.
+        </p>
+      </div>
+      <div className="parent">
+        {OurpromisesData.map((item, index) => {
+          return (
+            <div
+              className="child"
+              key={index}
+              style={{ borderTop: `4px solid ${item.color}` }}
+            >
+              <div className="title__parent">
+                <FontAwesomeIcon
+                  icon={item.icon}
+                  color={item.color}
+                  fontSize="30px"
+                />
+                <h1 className="title">{item.title}</h1>
+              </div>
+              <div>
+                <p className="para">{item.content}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </OurPromisesStyle>
+  );
+};
+export default OurPromisesComponent;
